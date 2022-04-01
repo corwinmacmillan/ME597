7d044588ac1015ca4ffeed167c0dc97f1cce76ea
corwinmacmillan <cgjmacmi@edu.uwaterloo.ca>
2022-04-01 02:38:33 -0400

    tired

--- src/cmotion/src/cmotion/main.py
+++ src/cmotion/src/cmotion/main.py
@@ -3,16 +3,20 @@
 #from cmotion.mround import *
 #from cmotion.Q2 import *
 import rospy
-# import rosbag
+import rosbag
 # from sensor_msgs.msg import PointCloud2
 # from sensor_msgs.msg import Image
+from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 from nav_msgs.msg import Path
 import tf
 import rospkg
-import subprocess
+# import subprocess
 import math
+import matplotlib.pyplot as plt
+from matplotlib import use
+use('TKAgg')
 
 
 class Map2Odom:
@@ -33,66 +37,48 @@ class Map2Odom:
         self.path.header.stamp = rospy.Time.now()
         self.path.header.frame_id = "odom"
         self.path.poses.append(pose)
-        od_msg = msg
-        pose = od_msg.pose.pose.position
-        orientation = od_msg.pose.pose.orientation
-        roll, pitch, yaw = self.euler_from_quaternion(orientation.x, orientation.y, orientation.z, orientation.w)
         self.path_publisher.publish(self.path)
 
-    def euler_from_quaternion(self, x, y, z, w):
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-
-        return roll_x, pitch_y, yaw_z  # in radians
 
 def Q2():
-    rospack = rospkg.RosPack()
-    CMotionDir = rospack.get_path('cmotion') + '/Bags/'
-    player_proc = subprocess.Popen(['rosbag', 'play', 'CircularMotionBag.bag'], cwd=CMotionDir)
+    rospy.init_node('Path_taken')
+    # rospack = rospkg.RosPack()
+    # CMotionDir = rospack.get_path('cmotion') + '/Bags/'
+    # player_proc = subprocess.Popen(['rosbag', 'play', 'CircularMotionBag.bag'], cwd=CMotionDir)
     pathPub = Map2Odom()
+    rospy.spin()
 
-    # pointCloudBag = rosbag.Bag(CMotionPath)
-    # #pathBag = rosbag.Bag('../../../../2022-03-09-17-40-02.bag')
-    # # IMUBag = rosbag.Bag('../../../../imu_odom.bag')
-    # rate = rospy.Rate(10)
-    # pointPub = rospy.Publisher('/camera/depth_registered/points', PointCloud2, queue_size=0)
-    # imagePub = rospy.Publisher('/camera/rgb/image_color', Image, queue_size=0)
-    # pathPub = rospy.Publisher('/path', Path, queue_size=10)
-    #
-    # path = Path()
-    # pose = PoseStamped()
-    # for topics, msgs, ts in zip(pointCloudBag.read_messages(topics=['/camera/depth_registered/points']),
-    #                             pointCloudBag.read_messages(topics=['/camera/rgb/image_color']),
-    #                             pointCloudBag.read_messages(topics=['/odom'])):
-    #     pointPub.publish(msgs[0])
-    #     imagePub.publish(msgs[1])
-    #
-    #     btf = tf.TransformBroadcaster()
-    #
-    #     pose.pose = msgs[2].pose.pose
-    #     pose.header.stamp = ts[2]
-    #     pose.header.frame_id = "odom"
-    #     btf.sendTransform((0, 0, 0), (0, 0, 0, 1), rospy.Time.now(), "map", "odom")
-    #     path.header.stamp = rospy.Time.now()
-    #     path.header.frame_id = "odom"
-    #     path.poses.append(pose)
-    #     pathPub.publish(path)
-    #
-    #     rate.sleep()
+
+def Q3():
+    rospack = rospkg.RosPack()
+    CMotionPath = rospack.get_path('cmotion') + '/Bags/CircularMotionBag.bag'
+    pointCloudBag = rosbag.Bag(CMotionPath)
+    #pathBag = rosbag.Bag('../../../../2022-03-09-17-40-02.bag')
+    # IMUBag = rosbag.Bag('../../../../imu_odom.bag')
+
+    imu_acceleration = []
+    imu_ang_vel = []
+    for topic, msg, t in pointCloudBag.read_messages(topics=['/imu']):
+        imu_acceleration.append([msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z])
+        imu_ang_vel.append([msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z])
+    plt.figure(1)
+    plt.plot(imu_acceleration[:, 0])
+    plt.plot(imu_acceleration[:, 1])
+    # plt.plot(imu_acceleration[:, 2])
+    plt.title('IMU Linear Acceleration')
+    plt.legend('x', 'y')
+    plt.figure(2)
+    # plt.plot(imu_ang_vel[:, 0])
+    # plt.plot(imu_ang_vel[:, 1])
+    plt.plot(imu_ang_vel[:, 2])
+    plt.title('IMU Angular Velocity')
+    plt.legend('yaw')
+    plt.show()
+
 
 
 if __name__ == "__main__":
     try:
-        Q2()
+        Q3()
     except rospy.ROSInterruptException:
         pass
